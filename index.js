03a825c19fa94000ed90e5d8b1c49bd54e953fbf
Roguecoder113 <kgodden15@gmail.com>
2022-03-05 23:22:38 -0800

    fixedexpress
    added a homepage fixed the index.ejs code

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.get('/', (req, res) => res.render('pages/homepage'))
+
+
 
 app.post('/update', (req, res) => {
   var getUserQuery = req.body.code;
@@ -18,3 +20,6 @@ app.post('/update', (req, res) => {
     res.sendStatus(200);
   })
 })
+
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
