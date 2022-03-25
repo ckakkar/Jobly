b3da9627ace7f955b1f6e206474a887709b55ccd
Sarthak-Kar <83546763+Sarthak-Kar@users.noreply.github.com>
2022-03-24 20:01:09 -0700

    Add files via upload

--- public/employer.js
+++ public/employer.js
@@ -6,6 +6,20 @@ function go_to_postedjobs() {
     location.href = "postedjobs.html?" + queryString;
 }
 
+async function logout() {
+    var str = { code: "truncate current;" };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(str),
+    };
+
+    var wait = await fetch('/update', options);
+    location.href = "index.html";
+}
+
 
 const queryString = location.search.substring(1);
-document.getElementById("welcome").innerHTML = "Welcome "+ queryString;
+document.getElementById("welcome").innerHTML = "Welcome " + queryString;
