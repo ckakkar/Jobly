49a5aa92154c71575e2eac31414faa4c9b17a3a1
ICasz <91713607+ICasz@users.noreply.github.com>
2022-04-06 21:42:56 -0700

    updated api search

--- public/api.js
+++ public/api.js
@@ -5,11 +5,7 @@ async function callApi() {
   var position = encodeURIComponent(document.getElementById('position').value.trim());
   var location = encodeURIComponent(document.getElementById('location').value.trim());
 
-  console.log(`Position = ${position}`);
-  console.log(`Location = ${location}`);
-
   var url = getUrl(position, location);
-  console.log(url);
 
   const options = {
     method: 'GET',
@@ -19,27 +15,85 @@ async function callApi() {
   }
   fetch(url, options)
   .then(response => response.json())
-  .then(data => displayJobs(data.results))
+  .then(data => displaySingleJob(data.results, 0))
   .catch (error => console.log(error));
 }
 
-function displayJobs(results) {
-  console.log(results);
-  document.getElementById('displayJobs').innerHTML = '';
-  for (let i = 0; i < results.length; i++) {
-    document.getElementById('displayJobs').innerHTML += `
-    <table classname="jobCard">
-      <p class="jobField"> Title: ${results[i].title} </p>
-      <p class="jobField"> Company: ${results[i].company.display_name} </p>
-      <p class="jobField"> Location: ${results[i].location.display_name} </p>
-      <p class="jobField"> Description: ${results[i].description} </p>
-      <a id="viewJob" href="${results[i].redirect_url}">View Job</a>
-      <br>
-    <table/>
-    `;
+function displaySingleJob(results, i) {
+  if (results.length == 0) {
+      document.getElementById('displayJobs').innerHTML = `<div id="jobCard"> No results. Please refine your search <div>`;
+      return;
   }
+
+  var html = "";
+  html += `<div id="jobCard">`
+    if (results[i].title != undefined) {
+      html += `<p id="title"> ${results[i].title} </p>`
+    }
+    if (results[i].company.display_name != undefined) {
+      html += `<p id="company"> ${results[i].company.display_name} </p>`
+    }
+    if (results[i].location.display_name != undefined) {
+      html += `<p id="location"> ${results[i].location.display_name} </p>`
+    }
+    if (results[i].description != undefined) {
+      html += `<p id="description"> ${results[i].description} </p>`
+    }
+    html += `</div>
+    <div id = "jobCardButtons">
+      <button class="jobCardButton" id="prevJob" type="button" name="button">Previous Job</button>
+      <a class="jobCardButton" id="viewJob" href="${results[i].redirect_url}">View Job</a>
+      <button class="jobCardButton" id="nextJob" type="button" name="button">Next Job</button>
+    </div>
+  `;
+
+  document.getElementById('displayJobs').innerHTML = html;
+
+  var nextJob = document.getElementById("nextJob");
+  var prevJob = document.getElementById("prevJob");
+
+  if (i == 0) {
+    prevJob.style.color = "gray";
+  }
+  if (i == results.length - 1) {
+    nextJob.style.color = "gray";
+  }
+
+  nextJob.onclick = function() {
+    if (i + 1 < results.length) {
+      displaySingleJob(results, i + 1)
+    }
+  };
+
+  prevJob.onclick = function() {
+    if (i > 0) {
+      displaySingleJob(results, i - 1);
+    }
+  };
 }
 
+// function displayJobs(results) {
+//
+//   if (results.length == 0) {
+//       document.getElementById('displayJobs').innerHTML = "No results. Please refine your search"
+//       return;
+//   }
+//
+//   document.getElementById('displayJobs').innerHTML = '';
+//   for (let i = 0; i < results.length; i++) {
+//     document.getElementById('displayJobs').innerHTML += `
+//     <table classname="jobCard">
+//       <p class="jobField"> Title: ${results[i].title} </p>
+//       <p class="jobField"> Company: ${results[i].company.display_name} </p>
+//       <p class="jobField"> Location: ${results[i].location.display_name} </p>
+//       <p class="jobField"> Description: ${results[i].description} </p>
+//       <a id="viewJob" href="${results[i].redirect_url}">View Job</a>
+//       <br>
+//     <table/>
+//     `;
+//   }
+// }
+
 function getUrl(position, location) {
-  return `https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=${app_id}&app_key=${app_key}&results_per_page=10&what=${position}&where=${location}`
+  return `https://api.adzuna.com/v1/api/jobs/ca/search/1?app_id=${app_id}&app_key=${app_key}&results_per_page=30&what=${position}&where=${location}`
 }
